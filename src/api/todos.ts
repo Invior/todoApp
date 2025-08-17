8d93dd48d99aff20afe4f62f245a6b51d994e824
Invior <twhlost@gmail.com>
2025-08-17 15:25:05 +0400

    Добавил пагинацию

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:3001';
 
-export const fetchTodos = async () => {
-    const response = await axios.get(`${API_URL}/todos?page=1&limit=10`);
+export const fetchTodos = async (page: number, limit: number) => {
+    const response = await axios.get(`${API_URL}/todos?page=${page}&limit=${limit}`);
     return response.data;
 };
 
